14c39027b94e159ffeb041599fea0fd72f86d895
jsytal1 <jsytal1@gmail.com>
2024-07-31 16:24:30 -0400

    refactor: update MaeApp to use Scaffold for better layout handling
    - added window insets handling

--- app/src/main/java/dev/syta/myaudioevents/ui/MaeApp.kt
+++ app/src/main/java/dev/syta/myaudioevents/ui/MaeApp.kt
@@ -1,10 +1,37 @@
 package dev.syta.myaudioevents.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import dev.syta.myaudioevents.navigation.MaeNavHost
 
 @Composable
-fun MaeApp(appState: MaeAppState) {
-    MaeNavHost(appState = appState)
+fun MaeApp(appState: MaeAppState, modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier,
+    ) { padding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .consumeWindowInsets(padding)
+                .windowInsetsPadding(
+                    WindowInsets.safeDrawing.only(
+                        WindowInsetsSides.Horizontal,
+                    ),
+                ),
+        ) {
+            MaeNavHost(appState = appState)
+        }
+    }
 }
 
