9954d40d2462e306dc330cc8c3abf49352a84ca3
jsytal1 <jsytal1@gmail.com>
2024-08-16 14:39:51 -0400

    feat: Display list of audio classes on Categories screen

--- app/src/main/java/dev/syta/myaudioevents/ui/screens/categories/CategoriesScreen.kt
+++ app/src/main/java/dev/syta/myaudioevents/ui/screens/categories/CategoriesScreen.kt
@@ -1,12 +1,39 @@
 package dev.syta.myaudioevents.ui.screens.categories
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
+import dev.syta.myaudioevents.data.model.AudioClass
 
 @Composable
 fun CategoriesScreen(
-    modifier: Modifier = Modifier,
     viewModel: CategoriesViewModel = hiltViewModel(),
 ) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    when (val s = uiState) {
+        CategoriesScreenUiState.Loading -> {
+            Text("Loading")
+        }
+
+        is CategoriesScreenUiState.Ready -> {
+            CategoriesList(
+                audioClassList = s.audioClassList,
+            )
+        }
+    }
 }
+
+@Composable
+fun CategoriesList(audioClassList: List<AudioClass>) {
+    LazyColumn {
+        items(audioClassList) { audioClass ->
+            Text(audioClass.name)
+        }
+    }
+}
+
--- app/src/main/java/dev/syta/myaudioevents/ui/screens/categories/CategoriesViewModel.kt
+++ app/src/main/java/dev/syta/myaudioevents/ui/screens/categories/CategoriesViewModel.kt
@@ -1,13 +1,41 @@
 package dev.syta.myaudioevents.ui.screens.categories
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.syta.myaudioevents.data.model.AudioClass
 import dev.syta.myaudioevents.data.repository.AudioClassStore
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class CategoriesViewModel @Inject constructor(
-    private val audioClassStore: AudioClassStore,
+    audioClassStore: AudioClassStore,
 ) : ViewModel() {
+    private val audioClassesFlow = audioClassStore.getAudioClasses()
+
+    val uiState = combine(
+        audioClassesFlow,
+    ) { (audioClasses) ->
+        if (audioClasses.isEmpty()) {
+            CategoriesScreenUiState.Loading
+        } else {
+            CategoriesScreenUiState.Ready(
+                audioClassList = audioClasses,
+            )
+        }
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        CategoriesScreenUiState.Loading,
+    )
 }
 
+sealed interface CategoriesScreenUiState {
+    data object Loading : CategoriesScreenUiState
+    data class Ready(
+        val audioClassList: List<AudioClass>,
+    ) : CategoriesScreenUiState
+}
