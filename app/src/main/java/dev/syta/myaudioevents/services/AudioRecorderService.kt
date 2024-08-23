10f28ab646955ef495b62eaf53ad819a7241c45d
jsytal1 <jsytal1@gmail.com>
2024-08-23 17:27:15 -0400

    feat: Add foreground service notification

--- app/src/main/java/dev/syta/myaudioevents/services/AudioRecorderService.kt
+++ app/src/main/java/dev/syta/myaudioevents/services/AudioRecorderService.kt
@@ -1,13 +1,21 @@
 package dev.syta.myaudioevents.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.media.MediaRecorder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dev.syta.myaudioevents.MainActivity
+import dev.syta.myaudioevents.R
 import dev.syta.myaudioevents.data.event.Events
 import dev.syta.myaudioevents.data.model.AudioRecording
 import dev.syta.myaudioevents.data.repository.AudioRecordingRepository
@@ -26,6 +34,11 @@ enum class AudioRecorderState {
     RECORDING, IDLE
 }
 
+const val FOREGROUND_NOTIFICATION_ID = 1293478424
+const val CHANNEL_ID = "AudioRecorderService"
+
+const val ACTION_STOP_RECORDING = "ACTION_STOP_RECORDING"
+
 @AndroidEntryPoint
 class AudioRecorderService : Service() {
     @ApplicationContext
@@ -37,6 +50,9 @@ class AudioRecorderService : Service() {
 
     companion object {
         var isRunning = false
+
+        const val STOP_RECORDING_REQUEST_CODE = 1
+        const val OPEN_APP_REQUEST_CODE = 2
     }
 
     private lateinit var baseDir: String
@@ -48,15 +64,32 @@ class AudioRecorderService : Service() {
     override fun onCreate() {
         super.onCreate()
         baseDir = "${context.getExternalFilesDir(null)}/$AUDIO_RECORDING_PATH"
+
+        createNotificationChannel()
     }
 
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val label = getString(R.string.app_name)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val notificationChannel = NotificationChannel(CHANNEL_ID, label, importance)
+            notificationChannel.setSound(null, null)
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+    }
 
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
         Log.d(LOGGER_TAG, "onStartCommand")
-        startRecording()
+        when (intent.action) {
+            ACTION_STOP_RECORDING -> stopRecording()
+            else -> startRecording()
+        }
         return START_NOT_STICKY
     }
 
@@ -64,6 +97,7 @@ class AudioRecorderService : Service() {
         Log.d(LOGGER_TAG, "onDestroy")
         super.onDestroy()
         stopRecording()
+        stopSelf()
         isRunning = false
     }
 
@@ -89,6 +123,7 @@ class AudioRecorderService : Service() {
                 start()
                 Log.d(LOGGER_TAG, "Starting recording")
                 audioRecorderState = AudioRecorderState.RECORDING
+                startForeground(FOREGROUND_NOTIFICATION_ID, showNotification())
             } catch (e: IOException) {
                 Log.e(LOGGER_TAG, "Failed to start recording: ${e.message}")
             }
@@ -96,19 +131,43 @@ class AudioRecorderService : Service() {
         broadcastStatus()
     }
 
-    private fun setupMediaRecorder(outputFilePath: String) {
-        recorder = MediaRecorder().apply {
-            setAudioSource(MediaRecorder.AudioSource.MIC)
-            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-            setOutputFile(outputFilePath)
-            try {
-                prepare()
-            } catch (e: IOException) {
-                Log.e(LOGGER_TAG, "MediaRecorder prepare() failed", e)
-            }
-        }
+    private fun showNotification(): Notification {
+        Log.d(LOGGER_TAG, "Showing notification")
+        return NotificationCompat.Builder(context, CHANNEL_ID).apply {
+            setSmallIcon(R.drawable.baseline_mic_24)
+            setContentTitle(getString(R.string.app_name))
+            setContentText(getString(R.string.recording_in_progress))
+            setContentIntent(getOpenAppIntent())
+            addAction(
+                NotificationCompat.Action(
+                    R.drawable.baseline_stop_24,
+                    getString(R.string.stop),
+                    getStopRecordingIntent()
+                )
+            )
+        }.build()
+    }
 
+    private fun getStopRecordingIntent(): PendingIntent {
+        val stopRecordingIntent = Intent(context, AudioRecorderService::class.java)
+        stopRecordingIntent.action = ACTION_STOP_RECORDING
+        return PendingIntent.getService(
+            context,
+            STOP_RECORDING_REQUEST_CODE,
+            stopRecordingIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+    }
+
+    private fun getOpenAppIntent(): PendingIntent {
+        val openAppIntent = Intent(context, MainActivity::class.java)
+        openAppIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+        return PendingIntent.getActivity(
+            context,
+            OPEN_APP_REQUEST_CODE,
+            openAppIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
     }
 
     private fun stopRecording() {
