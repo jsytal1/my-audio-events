9cfab48bc79895c1a4820621bf85f6841ea30be3
jsytal1 <jsytal1@gmail.com>
2024-08-26 22:14:52 -0400

    feat: Improve Label list UI
    - Adds spacing between items in the LazyColumn.
    - Updates the ListItem composable to use a Card with padding.
    - Adds a secondary constructor to the Label data class for easier creation.
    - Includes preview composables for ListItem and LabelList.

--- app/src/main/java/dev/syta/myaudioevents/data/model/Label.kt
+++ app/src/main/java/dev/syta/myaudioevents/data/model/Label.kt
@@ -3,4 +3,6 @@ package dev.syta.myaudioevents.data.model
 data class Label(
     val id: Int = 0,
     val name: String,
-)
+) {
+    constructor(name: String) : this(0, name)
+}
--- app/src/main/java/dev/syta/myaudioevents/ui/screens/label_list/LabelListScreen.kt
+++ app/src/main/java/dev/syta/myaudioevents/ui/screens/label_list/LabelListScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.syta.myaudioevents.R
@@ -93,7 +94,9 @@ fun AddLabelDialog(onDismiss: () -> Unit, onConfirm: (value: String) -> Unit) {
 
 @Composable
 fun LabelList(labelList: List<Label>) {
-    LazyColumn {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
         items(labelList) { label ->
             ListItem(
                 name = label.name,
@@ -104,10 +107,10 @@ fun LabelList(labelList: List<Label>) {
 
 @Composable
 fun ListItem(name: String) {
-    Card(
-        modifier = Modifier.padding(8.dp)
-    ) {
-        Row {
+    Card {
+        Row(
+            modifier = Modifier.padding(16.dp)
+        ) {
             Text(
                 modifier = Modifier.weight(1f),
                 text = name
@@ -115,6 +118,28 @@ fun ListItem(name: String) {
         }
     }
 }
+
+@Preview
+@Composable
+fun ListItemPreview() {
+    ListItem(
+        name = "Label 1"
+    )
+}
+
+@Preview
+@Composable
+fun LabelListPreview() {
+    LabelList(
+        labelList = listOf(
+            Label("Label 1"),
+            Label("Label 2"),
+            Label("Label 3"),
+        )
+    )
+}
+
+
 
 @Composable
 fun LabelsScreenContent(
